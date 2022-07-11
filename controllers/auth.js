4665793030b53dbff63dc18fe31df3802bfe0dbb
1Tolv2 <sofiaq1@gmail.com>
2022-07-11 15:40:08 +0200

    corrected changed code

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,9 @@
 const jwt = require("jsonwebtoken");
-const { verifyUser, createUser, getUserByUsername } = require("../models/user");
+const {
+  verifyUser,
+  createUser,
+  findUserByUsername,
+} = require("../models/user");
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const requireLogin = (req, res, next) => {
@@ -12,7 +16,7 @@ const registerNewUser = async (req, res) => {
   if (!(username && password)) {
     res.json({ error: "Invalid data. Username and password is required." });
   } else {
-    if (await getUserByUsername(username)) {
+    if (await findUserByUsername(username)) {
       res
         .status(400)
         .json({ error: "User already exists, username must be unique" });
@@ -29,8 +33,8 @@ const registerNewUser = async (req, res) => {
 
 const logInUser = async (req, res) => {
   const { username, password } = req.body;
-
   const user = await verifyUser(username.toLowerCase(), password);
+
   if (user) {
     const userId = user._id.toString();
     const token = jwt.sign({ userId, username: user.username }, JWT_SECRET, {
